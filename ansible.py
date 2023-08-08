594f6efa21d132d17052cffe7261db870735cfc4
Maksim Perov <coder@frtk.ru>
2023-08-08 19:57:28 +0300

    first steps to generate ansible playbooks

---
+++ ansible.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2023 Maksim Perov <coder@frtk.ru>
+#
+
+import sys
+from ruamel.yaml import YAML
+from ruamel.yaml.compat import StringIO
+
+FILE_PLAYBOOK = "playbook.yml"
+
+class CustomYAML(YAML):
+    def dump(self, data, stream=None, **kw):
+        inefficient = False
+        if stream is None:
+            inefficient = True
+            stream = StringIO()
+        YAML.dump(self, data, stream, **kw)
+        if inefficient:
+            return stream.getvalue()
+
+def getPingPlayBook():
+    content = [ # using list allows to prepend '-' (dash symbol)
+                {
+                    'name'  : 'Ping pong',
+                    'hosts' : 'all',
+                    'tasks' :
+                    [ # using list allows to prepend '-' (dash symbol)
+                        {
+                            'name' : 'Ping',
+                            'ping' : None,
+                        }
+                    ],
+                }
+              ]
+    yaml = CustomYAML()
+    yaml.explicit_start = True # --- at the beginning of yaml
+    return yaml.dump(content)
+
+def writePlayBook(playbook = getPingPlayBook()):
+    try:
+        with open(FILE_PLAYBOOK , "w") as f:
+            f.write(playbook)
+    except Exception as e:
+        print("ERROR: " + FILE_PLAYBOOK + " isn't writable!")
+        print(str(e))
+        sys.exit(-1)
+    print('YAML is successfully generating to ' + FILE_PLAYBOOK)
+    print('This file should be in your current directory!')
