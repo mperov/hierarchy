72a9bc2030bc4f438c41140cda121526f0e8ee3d
Maksim Perov <coder@frtk.ru>
2023-08-09 21:02:40 +0300

    walking on chown directory recursive

--- hierarchy.py
+++ hierarchy.py
@@ -5,7 +5,22 @@
 # Copyright (c) 2023 Maksim Perov <coder@frtk.ru>
 #
 
+import os
 from argparse import ArgumentParser
+
+def eprint(message):
+    print(message)
+    exit(-1)
+
+def walkLevel(some_dir, level=-1):
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        path = root.split(os.sep)
+        print(root)
+        num_sep_this = root.count(os.path.sep)
+        if level > -1:
+            if num_sep + level <= num_sep_this:
+                del dirs[:]
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -17,4 +32,11 @@ if __name__ == "__main__":
                         help="this option enables debug mode")
     args = parser.parse_args()
     debug = args.verbose
-    parser.print_help()
+    directory = args.dir
+    level = 0
+    if directory:
+        if not os.path.isdir(directory):
+            eprint(directory + " is not directory!")
+        walkLevel(directory)
+    else:
+        parser.print_help()
